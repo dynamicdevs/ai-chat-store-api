1ee51d689c939a159de757440d4c4a40b51db1bd
Abraxas-365 <luisfmiranda8@gmail.com>
2023-06-27 16:25:41 -0500

    refactoring

--- internal/chatbot/controller.go
+++ internal/chatbot/controller.go
@@ -1,54 +1,60 @@
 package chatbot
 
 import (
-	"context"
+	"log"
 
 	"github.com/Abraxas-365/commerce-chat/pkg/openia/chat"
 	"github.com/gofiber/fiber/v2"
 )
 
+// ControllerFactory initializes chatbot routes and handlers
 func ControllerFactory(fiberApp *fiber.App, conf Config) {
-	r := fiberApp.Group("/api")
+	apiGroup := fiberApp.Group("/api")
 
-	r.Post("/messages", func(c *fiber.Ctx) error {
-		ctx := context.Background()
+	// Handler for chatting with the store
+	apiGroup.Post("/messages", func(c *fiber.Ctx) error {
 		bot := New(conf)
-		client, err := bot.clientdb.GetById(ctx, 1) //when there are more users get the id form a bearer token
+		client, err := bot.clientdb.GetById(c.Context(), 1)
 		if err != nil {
-			return c.Status(500).SendString("Failed to get client: " + err.Error())
+			log.Printf("Failed to get client: %v", err)
+			return c.Status(500).JSON(fiber.Map{"error": "Internal Server Error"})
 		}
 		var messages []chat.Message
 		if err := c.BodyParser(&messages); err != nil {
-			return c.Status(400).SendString("Failed to parse request")
+			return c.Status(400).JSON(fiber.Map{"error": "Invalid input format"})
 		}
 
 		bot.assistant.AddSystemPrompt(client.SystemPromt)
 		resp, err := bot.ChatAllTheStore(messages)
 		if err != nil {
-			return c.Status(400).SendString("Failed to get message " + err.Error())
+			log.Printf("Failed to get message: %v", err)
+			return c.Status(400).JSON(fiber.Map{"error": "Bad Request"})
 		}
 
 		return c.JSON(resp)
-
 	})
 
-	r.Post("/messages/product/:id", func(c *fiber.Ctx) error {
-		ctx := context.Background()
+	// Handler for chatting with a specific product
+	apiGroup.Post("/products/:id/messages", func(c *fiber.Ctx) error {
 		bot := New(conf)
-		client, err := bot.clientdb.GetById(ctx, 1) //when there are more users get the id form a bearer token
-		var messages []chat.Message
-		sku := c.Params("id")
-		if err := c.BodyParser(&messages); err != nil {
-			return c.Status(400).SendString("Failed to parse request")
+		client, err := bot.clientdb.GetById(c.Context(), 1)
+		if err != nil {
+			log.Printf("Failed to get client: %v", err)
+			return c.Status(500).JSON(fiber.Map{"error": "Internal Server Error"})
 		}
+		var messages []chat.Message
+		if err := c.BodyParser(&messages); err != nil {
+			return c.Status(400).JSON(fiber.Map{"error": "Invalid input format"})
+		}
+		sku := c.Params("id")
 
 		bot.assistant.AddSystemPrompt(client.SystemPromt)
 		resp, err := bot.ChatWithProduct(sku, messages)
 		if err != nil {
-			return c.Status(400).SendString("Failed to get message " + err.Error())
+			log.Printf("Failed to get message: %v", err)
+			return c.Status(400).JSON(fiber.Map{"error": "Bad Request"})
 		}
 
 		return c.JSON(resp)
-
 	})
 }
